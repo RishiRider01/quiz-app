663ba9a741033d8c1f6b509dec90ed9e1d831300
Saptarshi Ghosh <sptrshighsh01@gmail.com>
2021-07-06 20:21:10 +0530

    newBG

--- lib/Page/quiz_page.dart
+++ lib/Page/quiz_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:quiz_app/Widget/app_name.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz_questions.dart';
@@ -15,21 +16,22 @@ class _QuizPageState extends State<QuizPage> {
     bool correctAnswer = questions.getAnswer();
     setState(
       () {
-        if (correctAnswer == answer) {
-          if (scoreKeeper.length < questions.getLength())
+        if (scoreKeeper.length < questions.getLength()) {
+          if (correctAnswer == answer) {
             scoreKeeper.add(
               Icon(
                 Icons.check,
                 color: Colors.green,
               ),
             );
-        } else {
-          scoreKeeper.add(
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          );
+          } else {
+            scoreKeeper.add(
+              Icon(
+                Icons.close,
+                color: Colors.red,
+              ),
+            );
+          }
         }
         questions.nextQuestion();
       },
@@ -44,73 +46,82 @@ class _QuizPageState extends State<QuizPage> {
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            //questions
-            flex: 5,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Center(
-                child: Text(
-                  questions.getQuestionText(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.black54,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/bg/question.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.6), BlendMode.darken)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              //questions
+              flex: 5,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Center(
+                  child: Text(
+                    questions.getQuestionText(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: FlatButton(
-                    color: Colors.green,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: FlatButton(
+                      color: Colors.green,
+                      child: Text(
+                        'true',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        checkAnswer(true);
+                      }),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: FlatButton(
+                    color: Colors.red,
                     child: Text(
-                      'true',
+                      'false',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20.0,
                       ),
                     ),
                     onPressed: () {
-                      checkAnswer(true);
-                    }),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: FlatButton(
-                  color: Colors.red,
-                  child: Text(
-                    'false',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                    ),
+                      checkAnswer(false);
+                    },
                   ),
-                  onPressed: () {
-                    checkAnswer(false);
-                  },
                 ),
               ),
             ),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: scoreKeeper,
-          ),
-        ],
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: scoreKeeper,
+            ),
+          ],
+        ),
       ),
     );
   }
